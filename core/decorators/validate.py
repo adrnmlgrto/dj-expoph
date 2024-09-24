e5691ce25c4c39005e56ce511fbde7857ab8f641
Adrian Melegrito <adrnmlgrto.dev@gmail.com>
2024-09-25 01:18:48 +0800

    Removed debug statements on decorator

--- core/decorators/validate.py
+++ core/decorators/validate.py
@@ -2,7 +2,6 @@ import inspect
 from functools import wraps
 from typing import Type
 
-from loguru import logger
 from pydantic import BaseModel
 
 
@@ -25,18 +24,11 @@ def validate_params(model: Type[BaseModel]):
             func_signature = inspect.signature(func)
             param_names = list(func_signature.parameters.keys())
 
-            # logger.debug(f'Param Names: {param_names}')
-
             # Map positional arguments to the corresponding parameter names.
             params = {**dict(zip(param_names, args)), **kwargs}
 
-            logger.debug(f'Data Parameters: {params}')
-
             # Validate the combined parameters using the Pydantic model.
             validated_data = model(**params).model_dump()
-
-            # logger.debug(f'Validated Data Dictionary: {params}')
-            # logger.debug(f'Function: {func.__name__}')
 
             # Call the original function with validated data.
             return func(**validated_data)
