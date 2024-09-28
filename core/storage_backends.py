fdad34b85a123e976c34e6d063acbdf1bab99308
Adrian Melegrito <adrnmlgrto.dev@gmail.com>
2024-09-29 00:49:43 +0800

    Fix issue on path being set to string literal.

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -3,6 +3,7 @@ from typing import override
 
 from loguru import logger
 from storages.backends.s3 import S3Storage
+from storages.utils import clean_name
 from supabase import Client, create_client
 
 
@@ -15,9 +16,12 @@ class SupabaseS3Storage(S3Storage):
     Overridden storage class for S3-compatible buckets on Supabase.
     """
     @override
-    def url(self, *args, **kwargs):
+    def url(self, name, parameters=None, expire=None, http_method=None):
 
         try:
+
+            # Normalize and clean the name of the file from params.
+            name = self._normalize_name(clean_name(name))
 
             # Initialize the `supabase` client.
             sb: Client = create_client(
@@ -25,9 +29,9 @@ class SupabaseS3Storage(S3Storage):
                 supabase_key=os.getenv('SUPABASE_API_KEY')
             )
             # Retrieve the "signedURL" from the response.
+            # NOTE: `S3Storage` defaults to 3600s for expire.
             response = sb.storage.from_('expoph-bucket').create_signed_url(
-                path='users/Sakuuu/avatar.png',
-                expires_in=self.querystring_expire  # 3600s from S3Storage
+                path=name, expires_in=self.querystring_expire
             )
             url = response.get('signedURL', None)
 
@@ -44,4 +48,4 @@ class SupabaseS3Storage(S3Storage):
                 f'Error retrieving supabase-signed url: {e}'
             )
             # Call the original `S3Storage.url()` as fallback.
-            return super().url(*args, **kwargs)
+            return super().url(name, parameters, expire, http_method)
